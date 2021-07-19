47d13b53089a8c43b79d8b8b329fe26f60b839fa
sharlq <shehabakhader@gmail.com>
2021-07-19 20:58:46 +0300

    created about page and applied routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router'
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
-import { HttpClientModule } from '@angular/common/http';
 import { AddTaskComponent } from './components/add-task/add-task.component';
-import {FormsModule} from '@angular/forms'
+import { AboutComponent } from './components/about/about.component';
+
+const appRoutes:Routes =[
+  {path: '', component:TasksComponent},
+  {path: 'about', component:AboutComponent}
+]
 
 @NgModule({
   declarations: [
@@ -18,14 +26,16 @@ import {FormsModule} from '@angular/forms'
     ButtonComponent,
     TasksComponent,
     TaskItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes,{enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
