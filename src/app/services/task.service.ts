b2dc5abaa88bf3549f5525c58f3814272c0ec382
sharlq <shehabakhader@gmail.com>
2021-07-19 19:26:09 +0300

    created the functionality of adding tasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient,HttpHeaders} from '@angular/common/http'
 import {Observable,of} from 'rxjs'
 import { Task } from 'src/app/task';
-import { TASKS } from 'src/app/mock-Tasks';
+
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -13,6 +13,7 @@ const httpOptions = {
 @Injectable({
   providedIn: 'root'
 })
+
 export class TaskService {
   private apiUrl = 'http://localhost:5000/tasks'
   constructor(private http:HttpClient) { }
@@ -29,5 +30,8 @@ export class TaskService {
   updateTaskReminder(task:Task):Observable<Task>{
     const url = `${this.apiUrl}/${task.id}`
     return this.http.put<Task>(url,task,httpOptions) 
+  }
+  addTask(newTask:Task){
+    return this.http.post<Task>(this.apiUrl,newTask,httpOptions)
   }
 }
